f5c7e18af4bc703389418ce84511ffe0e1ee23ff
Antonio Gennaro <antgennaro@hotmail.com>
2023-03-16 16:47:28 -0300

    Primer Commit

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector("body").innerHTML = `
+<h1>Header Tamaño 1</h1>
+<h2>Header Tamaño 2</h2>
+<h3>Header Tamaño 3</h3>
+<h4>CANICHE TOY DESDE ARCHIVO EN CARPETA "MEDIA"</h4>
+<img src="Media/Toy.jfif" alt="caniche toy blanco">
+</img><h4> OTRO CANICHE TOY DESDE LINK EXTERNO</h4>
+< img src = "https://http2.mlstatic.com/D_NQ_NP_2X_991408-MLA49037315188_022022-F.webp" alt = "otro caniche toy chocolate" >
+<h4> upss!!! me salió muy grande...</h4>
+<h4>"VA MAS CHICO EL CHOCOLATE</h4>
+<img width="150" height="200" src="https://http2.mlstatic.com/D_NQ_NP_2X_991408-MLA49037315188_022022-F.webp"alt="otro caniche toy chocolate más chico">';
+`
