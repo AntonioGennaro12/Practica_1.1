177e0bc63c9894277f454551c768efa2b20196ef
Antonio Gennaro <antgennaro@hotmail.com>
2023-03-17 16:07:12 -0300

    va con cambios, no anda la ultima img

--- algoritmo.js
+++ algoritmo.js
@@ -7,8 +7,8 @@ document.querySelector("body").innerHTML = `
     <h4> OTRO CANICHE TOY DESDE LINK EXTERNO</h4>
     <img src="https://http2.mlstatic.com/D_NQ_NP_2X_991408-MLA49037315188_022022-F.webp"
         alt="otro caniche toy chocolate">
-        <h4> upss!!! me salió muy grande...</h4>
-        <h4>"VA MAS CHICO EL CHOCOLATE</h4>
+    <h4> upss!!! me salió muy grande...</h4>
+    <h4>VA MAS CHICO EL CHOCOLATE</h4>
     <img width="150" height="200" src="https://http2.mlstatic.com/D_NQ_NP_2X_991408-MLA49037315188_022022-F.webp"
-        alt="otro caniche toy chocolate más chico"
+        alt="otro chocolate más chico"
 `
